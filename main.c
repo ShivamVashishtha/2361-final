360bfb72c555596ce5927e50813a0a1907811a29
Shivam Vashishtha <58797316+ShivamVashishtha@users.noreply.github.com>
2025-04-21 14:47:41 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,38 +4,35 @@
 #include "uart.h"
 #include <stdio.h>
 
-// CW1: FLASH CONFIGURATION WORD 1 (see PIC24 Family Reference Manual 24.1)
-#pragma config ICS = PGx1          // Comm Channel Select (Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1)
-#pragma config FWDTEN = OFF        // Watchdog Timer Enable (Watchdog Timer is disabled)
-#pragma config GWRP = OFF          // General Code Segment Write Protect (Writes to program memory are allowed)
-#pragma config GCP = OFF           // General Code Segment Code Protect (Code protection is disabled)
-#pragma config JTAGEN = OFF        // JTAG Port Enable (JTAG port is disabled)
+// CW1: FLASH CONFIGURATION WORD 1
+#pragma config ICS = PGx1
+#pragma config FWDTEN = OFF
+#pragma config GWRP = OFF
+#pragma config GCP = OFF
+#pragma config JTAGEN = OFF
 
-
-// CW2: FLASH CONFIGURATION WORD 2 (see PIC24 Family Reference Manual 24.1)
-#pragma config I2C1SEL = PRI       // I2C1 Pin Location Select (Use default SCL1/SDA1 pins)
-#pragma config IOL1WAY = OFF       // IOLOCK Protection (IOLOCK may be changed via unlocking seq)
-#pragma config OSCIOFNC = ON       // Primary Oscillator I/O Function (CLKO/RC15 functions as I/O pin)
-#pragma config FCKSM = CSECME      // Clock Switching and Monitor (Clock switching is enabled, 
-                                       // Fail-Safe Clock Monitor is enabled)
-#pragma config FNOSC = FRCPLL      // Oscillator Select (Fast RC Oscillator with PLL module (FRCPLL))
+// CW2: FLASH CONFIGURATION WORD 2
+#pragma config I2C1SEL = PRI
+#pragma config IOL1WAY = OFF
+#pragma config OSCIOFNC = ON
+#pragma config FCKSM = CSECME
+#pragma config FNOSC = FRCPLL
 
 void setup(void)
 {
-    CLKDIVbits.RCDIV = 0;  // 1:1 clock division (16 MHz FCY)
+    CLKDIVbits.RCDIV = 0;
 
-    __builtin_write_OSCCONL(OSCCON & 0xBF);      // Unlock PPS
-    RPINR20bits.SDA1R = 9;                       // RB9 = SDA1 (input)
-    RPOR4bits.RP8R = 0b1000;                     // RB8 = SCL1 (output)
-    __builtin_write_OSCCONL(OSCCON | 0x40);      // Lock PPS
-
+    __builtin_write_OSCCONL(OSCCON & 0xBF);  // Unlock PPS
+    RPINR20bits.SDA1R = 9;                  // SDA1 on RB9
+    RPOR4bits.RP8R = 0b1000;                // SCL1 on RB8
+    __builtin_write_OSCCONL(OSCCON | 0x40); // Lock PPS
 
     neopixel_init();
     setLeds(1);
     setBrightness(50);
     clear();
 
-    I2C1_Init();    // Assuming I2C1_Init() is defined elsewhere
+    I2C1_Init();
     IMU_init();
     UART1_Init();
 }
@@ -45,25 +42,31 @@ char buffer[64];
 int main(void)
 {
     IMU_Data imu;
-    Gesture g;
+    Gesture g, last = GESTURE_NONE;
+
     setup();
 
     while (1) {
         IMU_read(&imu);
         g = detectGesture(&imu);
 
-        switch (g) {
-            case GESTURE_UP: setStrip(0, 255, 0); break;
-            case GESTURE_DOWN: setStrip(255, 0, 0); break;
-            case GESTURE_LEFT: setStrip(0, 0, 255); break;
-            case GESTURE_RIGHT: setStrip(255, 255, 0); break;
-            default: setStrip(0, 0, 0); break;
+        if (g != last) {
+            last = g;
+
+            switch (g) {
+                case GESTURE_UP:    setStrip(0, 255, 0); break;
+                case GESTURE_DOWN:  setStrip(255, 0, 0); break;
+                case GESTURE_LEFT:  setStrip(0, 0, 255); break;
+                case GESTURE_RIGHT: setStrip(255, 255, 0); break;
+                default:            setStrip(0, 0, 0); break;
+            }
+
+            show(); // Only update LEDs if gesture changed
         }
-        show();
 
         sprintf(buffer, "X: %.2f Y: %.2f Z: %.2f\r\n", imu.accel_x, imu.accel_y, imu.accel_z);
         UART1_Print(buffer);
 
-        __delay_ms(300);
+        __delay_ms(50); // Faster response
     }
 }
